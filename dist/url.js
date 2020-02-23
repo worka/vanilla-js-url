265fd0f7eb71571102e5538a4f88f482623798ee
worka <bckworka@gmail.com>
2020-02-23 22:37:07 +1000

    refactoring

--- dist/url.js
+++ dist/url.js
@@ -7,72 +7,102 @@
 })(this, function() {
     'use strict';
 
-    function getParams(url) {
-        url = url || window.location.href;
+    function _buildParams(query) {
         var params = {};
-        var splitUrl = url.split('?', 2);
-        var queries = splitUrl.length === 2 ? splitUrl[1].split('&') : [];
-        queries.forEach(function(query) {
-            var row = query.split('=', 2);
-            var key = row[0];
-            var value = row[1] || '';
 
-            if (key.substr(-2) === '[]') {
-                key = key.substr(0, key.length - 2);
+        if (query) {
+            query.split('&').forEach(function(_query) {
+                var row = _query.split('=', 2);
 
-                if (params[key] === undefined) {
-                    params[key] = [];
+                var key = row[0];
+                var value = row[1] || '';
+
+                if (key.substr(-2) === '[]') {
+                    key = key.substr(0, key.length - 2);
+
+                    if (params[key] === undefined) {
+                        params[key] = [];
+                    }
+
+                    params[key].push(decodeURIComponent(value));
+                } else {
+                    params[key] = decodeURIComponent(value);
                 }
+            });
+        }
 
-                params[key].push(decodeURIComponent(value));
-            } else {
-                params[key] = decodeURIComponent(value);
-            }
-        });
         return params;
+    }
+
+    function _buildQuery(params) {
+        var queries = [];
+
+        var _loop = function _loop(key) {
+            if (params.hasOwnProperty(key)) {
+                var value = params[key];
+
+                if (Array.isArray(value) && value.length) {
+                    value.forEach(function(_value) {
+                        queries.push(''.concat(key, '[]=').concat(_value));
+                    });
+                } else {
+                    queries.push(''.concat(key, '=').concat(value));
+                }
+            }
+        };
+
+        for (var key in params) {
+            _loop(key);
+        }
+
+        return queries.join('&');
+    }
+
+    function _concat(currentObject, newObject) {
+        var _loop2 = function _loop2(key) {
+            if (newObject.hasOwnProperty(key)) {
+                var value = newObject[key];
+
+                if (Array.isArray(value) && value.length) {
+                    if (
+                        currentObject[key] === undefined ||
+                        !Array.isArray(currentObject[key])
+                    ) {
+                        currentObject[key] = [];
+                    }
+
+                    value.forEach(function(_value) {
+                        currentObject[key].push(_value);
+                    });
+                } else {
+                    currentObject[key] = newObject[key];
+                }
+            }
+        };
+
+        for (var key in newObject) {
+            _loop2(key);
+        }
+
+        return currentObject;
+    }
+
+    function getParams() {
+        var url =
+            arguments.length > 0 && arguments[0] !== undefined
+                ? arguments[0]
+                : window.location.href;
+        var splitUrl = url.split('?', 2);
+        return _buildParams(splitUrl.length === 2 ? splitUrl[1] : '');
     }
 
     function addParams(url, newParams) {
         if (newParams instanceof Object) {
-            var uri = url.split('?', 2)[0];
-            var currentParams = getParams(url);
+            var uri = url.split('?', 2)[0]; // concatenating current params with new params
 
-            for (var key in newParams) {
-                if (newParams.hasOwnProperty(key)) {
-                    var value = newParams[key];
+            var params = _concat(getParams(url), newParams);
 
-                    if (Array.isArray(value) && value.length) {
-                        if (
-                            currentParams[key] === undefined ||
-                            !Array.isArray(currentParams[key])
-                        ) {
-                            currentParams[key] = [];
-                        }
-
-                        for (var i = 0; i < value.length; i++) {
-                            currentParams[key].push(value[i]);
-                        }
-                    } else {
-                        currentParams[key] = newParams[key];
-                    }
-                }
-            }
-
-            var queries = [];
-
-            for (var _key in currentParams) {
-                var _value = currentParams[_key];
-
-                if (Array.isArray(_value) && _value.length) {
-                    for (var j = 0; j < _value.length; j++) {
-                        queries.push(''.concat(_key, '[]=').concat(_value[j]));
-                    }
-                } else {
-                    queries.push(''.concat(_key, '=').concat(_value));
-                }
-            }
-
-            url = ''.concat(uri, '?').concat(queries.join('&'));
+            url = ''.concat(uri, '?').concat(_buildQuery(params));
         }
 
         return url;
--- src/url.js
+++ src/url.js
@@ -1,73 +1,88 @@
-function getParams(url) {
-    url = url || window.location.href;
-
+function _buildParams(query) {
     const params = {};
-    const splitUrl = url.split('?', 2);
-    const queries = splitUrl.length === 2 ? splitUrl[1].split('&') : [];
 
-    queries.forEach(query => {
-        const row = query.split('=', 2);
+    if (query) {
+        query.split('&').forEach(_query => {
+            const row = _query.split('=', 2);
 
-        let key = row[0];
-        let value = row[1] || '';
+            let key = row[0];
+            let value = row[1] || '';
 
-        if (key.substr(-2) === '[]') {
-            key = key.substr(0, key.length - 2);
+            if (key.substr(-2) === '[]') {
+                key = key.substr(0, key.length - 2);
 
-            if (params[key] === undefined) {
-                params[key] = [];
+                if (params[key] === undefined) {
+                    params[key] = [];
+                }
+
+                params[key].push(decodeURIComponent(value));
+            } else {
+                params[key] = decodeURIComponent(value);
             }
-
-            params[key].push(decodeURIComponent(value));
-        } else {
-            params[key] = decodeURIComponent(value);
-        }
-    });
+        });
+    }
 
     return params;
+}
+
+function _buildQuery(params) {
+    const queries = [];
+
+    for (let key in params) {
+        if (params.hasOwnProperty(key)) {
+            const value = params[key];
+
+            if (Array.isArray(value) && value.length) {
+                value.forEach(_value => {
+                    queries.push(`${key}[]=${_value}`);
+                });
+            } else {
+                queries.push(`${key}=${value}`);
+            }
+        }
+    }
+
+    return queries.join('&');
+}
+
+function _concat(currentObject, newObject) {
+    for (let key in newObject) {
+        if (newObject.hasOwnProperty(key)) {
+            const value = newObject[key];
+
+            if (Array.isArray(value) && value.length) {
+                if (
+                    currentObject[key] === undefined ||
+                    !Array.isArray(currentObject[key])
+                ) {
+                    currentObject[key] = [];
+                }
+
+                value.forEach(_value => {
+                    currentObject[key].push(_value);
+                });
+            } else {
+                currentObject[key] = newObject[key];
+            }
+        }
+    }
+
+    return currentObject;
+}
+
+function getParams(url = window.location.href) {
+    const splitUrl = url.split('?', 2);
+
+    return _buildParams(splitUrl.length === 2 ? splitUrl[1] : '');
 }
 
 function addParams(url, newParams) {
     if (newParams instanceof Object) {
         const uri = url.split('?', 2)[0];
-        const currentParams = getParams(url);
+        // concatenating current params with new params
+        const params = _concat(getParams(url), newParams);
 
-        for (let key in newParams) {
-            if (newParams.hasOwnProperty(key)) {
-                const value = newParams[key];
-
-                if (Array.isArray(value) && value.length) {
-                    if (
-                        currentParams[key] === undefined ||
-                        !Array.isArray(currentParams[key])
-                    ) {
-                        currentParams[key] = [];
-                    }
-
-                    for (let i = 0; i < value.length; i++) {
-                        currentParams[key].push(value[i]);
-                    }
-                } else {
-                    currentParams[key] = newParams[key];
-                }
-            }
-        }
-
-        const queries = [];
-
-        for (let key in currentParams) {
-            const value = currentParams[key];
-
-            if (Array.isArray(value) && value.length) {
-                for (let j = 0; j < value.length; j++) {
-                    queries.push(`${key}[]=${value[j]}`);
-                }
-            } else {
-                queries.push(`${key}=${value}`);
-            }
-        }
-
-        url = `${uri}?${queries.join('&')}`;
+        url = `${uri}?${_buildQuery(params)}`;
     }
 
     return url;
