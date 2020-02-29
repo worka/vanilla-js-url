9952767edf863e2a1bc4d78c0a11b03dbb1d35db
worka <bckworka@gmail.com>
2020-02-29 12:16:35 +1000

    update

--- src/_buildQueryDeep.js
+++ src/_buildQueryDeep.js
@@ -1,5 +1,20 @@
-export default function _buildQueryDeep(params) {
+export default function _buildQueryDeep(params, branch = [], tree = []) {
 
-    console.log(params);
+    for (let key of Object.keys(params)) {
+        branch.push(key);
 
+        if (params[key] instanceof Object) {
+            branch = _buildQueryDeep(params[key], branch);
+        } else {
+            branch.push(params[key]);
+            tree.push(branch);
+
+            return tree;
+        }
+    }
+
+
+    console.log(params, branch);
+
+    return tree;
 }
